54e58f38083a5fff4f17540f1d0fef9b9d074dc1
Swen Mulderij <swenmulderij@gmail.com>
2020-06-10 21:47:14 +0200

    We have a circle.
    Co-authored-by: julien Lengrand-Lambert <julien@lengrand.fr>

--- src/Main.elm
+++ src/Main.elm
@@ -11,17 +11,73 @@ import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
+import Svg
+import Svg.Attributes exposing (cx, cy, height, r, width)
 import Task
 
 
 type alias Model =
-    { val : String
+    { tick : Int
+    , sheeps : Flock
+    , dog : Dog
+    , target : Target
+    , gameSettings : GameSettings
     }
+
+
+type alias GameSettings =
+    { size : ( Int, Int )
+    }
+
+
+type Component
+    = KeyboardComponent
+    | SpriteComponent
+    | AreaComponent Int Int Int Int
+    | ScoreComponent
+    | LocationComponent Int Int
+
+
+type alias Entity =
+    List Component
+
+
+type alias Dog =
+    Entity
+
+
+type alias Sheep =
+    Entity
+
+
+type alias Target =
+    Entity
+
+
+type alias Flock =
+    List Sheep
+
+
+
+-- Entity : { position: 2DPoint, velocity: Vector, components:List component }
+-- update =
+-- for every entity
+--   new_velocity = f(velocity)
+--   postion = position + new_velocity
+--   f : Vector -> Vector
+--   avoidDog : Dog -> Vector -> Vector
+--   align : List Sheep -> Vector -> Vector
+--   seperate : List Sheep -> Vector -> Vector
+--   { sheep | velocity = avoidDog model.dog <| align model.sheeps <| seperate model.Sheep}
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { val = "Hello, world!"
+    ( { tick = 0
+      , sheeps = [ [ LocationComponent 100 200 ], [ LocationComponent 300 400 ] ]
+      , dog = [ LocationComponent 50 50 ]
+      , target = [ AreaComponent 50 50 100 100 ]
+      , gameSettings = { size = ( 600, 600 ) }
       }
     , Cmd.none
     )
@@ -58,17 +114,29 @@ update msg model =
 
 view : Model -> Document Msg
 view model =
-    {title="Sheepgame"
+    { title = "Sheepgame"
     , body =
-    [Element.layout
-        [ Font.family
-            [ Font.sansSerif
+        [ Element.layout
+            [ Font.family
+                [ Font.sansSerif
+                ]
+            , Element.width fill
             ]
-        , width fill
+          <|
+            Element.el
+                [ centerX
+                ]
+            <|
+                Element.html <|
+                    gameView model
         ]
-    <|
-        Element.el
-            [ centerX
-            ]
-            <| Element.text model.val
-    ]}
+    }
+
+
+gameView : Model -> Html Msg
+gameView model =
+    Svg.svg
+        [ Svg.Attributes.height <| String.fromInt <| Tuple.first model.gameSettings.size
+        , Svg.Attributes.width <| String.fromInt <| Tuple.second model.gameSettings.size
+        ]
+        [ Svg.circle [ cx "60", cy "60", r "50" ] [] ]
